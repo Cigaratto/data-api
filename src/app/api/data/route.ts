de1b8ed6e33b08a8819933d3a4d51a95abd37ea4
Cigaratto <build46@hotmail.com>
2025-03-31 00:45:16 +0700

    bug fixed

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,7 +1,7 @@
 import { aggregateData } from "@/app/utils/utils";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
+export const GET = async () => {
   const response = await fetch("https://dummyjson.com/users");
   const json = await response.json();
   const data = aggregateData(json.users);
