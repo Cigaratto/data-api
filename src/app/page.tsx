f4355582b2435643057f3c62573530fad16fb2b4
Cigaratto <build46@hotmail.com>
2025-04-03 15:54:14 +0700

    visualized create data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Page() {
+import { aggregateData } from "./utils/utils";
+
+export default async function Page() {
+const preTransformData = await fetch("https://dummyjson.com/users");
+const json = await preTransformData.json();
     return (
-      <div>
-        <h1>Page</h1>       
+      <div className="h-screen">
+        <h1 className="font-bold">Create data from API</h1>       
+       <div className="grid md:grid-cols-2 gap-4 h-full">
+       <div className="overflow-y-auto">
+          <h2>Before transform</h2>
+          <code>
+            <pre>
+            {JSON.stringify(json, null, 2)}
+            </pre>
+          </code>
+        </div>
+
+        <div className="overflow-y-auto">
+          <h2>After transform</h2>
+          <code>
+            <pre>
+            {JSON.stringify(aggregateData(json.users), null, 2)}
+            </pre>
+          </code>
+        </div>
+       </div>
       </div>
     );
 }
